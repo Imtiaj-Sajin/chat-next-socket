146746fd9f57a5f0f4c4ec2d07d3f5bd9a5ba814
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-11-28 19:57:41 +0600

    new origin

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 // Create a Socket.IO server
 const io = new Server(3001, {
   cors: {
-    origin: ["https://chat-next-socket-5k6dsbkle-imtiajsajins-projects.vercel.app","https://5a80-43-230-122-109.ngrok-free.app","http://localhost:3000"], // Allow requests from your Next.js app
+    origin: ["https://chat-next-socket-5k6dsbkle-imtiajsajins-projects.vercel.app","https://chat-next-socket.vercel.app/","https://5a80-43-230-122-109.ngrok-free.app","https://web-chat-next-socket.onrender.com/","http://localhost:3000"], // Allow requests from your Next.js app
     methods: ["GET", "POST"],
   },
 });
